e8c2d35f594f759da63ca46bdf6187ff4413c72b
Daniel Somers <d.somers@ulcc.ac.uk>
2012-10-15 14:55:58 +0100

    fixed issue with erroring log entries

--- lib.php
+++ lib.php
@@ -228,6 +228,10 @@ function get_recent_update_records($update_type) {
                 
                 if($log->action=='add mod') {
                 
+                    if(!$instanceid) {
+                        continue;
+                    }
+                
                     $cm = $modinfo->instances[$modname][$instanceid];
                     
                     // check if user has access
